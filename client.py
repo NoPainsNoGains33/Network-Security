b2a076eaae36bb29261827d6edb43055dc02a717
Yushen <ni.yus@husky.neu.edu>
2019-11-29 23:45:12 -0500

    Finish Kas encrypt and sign

--- client.py
+++ client.py
@@ -1,11 +1,17 @@
 import socket
 import zmq
+import sys
 from hashlib import sha256
 from message_type_pb2 import COMM_MESSAGE
+from diffiehellman.diffiehellman import DiffieHellman
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.asymmetric import padding as paddings
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+
 class Client():
-    client_name = " "
-    client_password = " "
-    # client_response = " "
 
     def __init__(self, c_name, c_password):
         self.client_name = c_name
@@ -18,7 +24,7 @@ class Client():
         return socket
 
 
-    def login(self, socket):
+    def puzzle(self, socket):
         Message_send = COMM_MESSAGE ()
         Message_send.type = COMM_MESSAGE.TYPE.LOGIN
         Message_rec = COMM_MESSAGE ()
@@ -29,23 +35,106 @@ class Client():
             digest = sha256()
             digest.update(str(i).encode())
             if Message_rec.N1_hash == digest.hexdigest():
-                return i
+                return  i
+
+    def session(self, socket, puzzle):
+        # set up the session key of client
+        Message_send = COMM_MESSAGE ()
+        Message_send.type = COMM_MESSAGE.TYPE.LOGIN
+        Message_rec =  COMM_MESSAGE ()
+        Message_send.N1  = puzzle
+        alice = DiffieHellman (group=5, key_length=200)
+        alice.generate_public_key()
+        # Message_send.ga_mod_p = str (alice.public_key)
+        Message_send.message = str (alice.public_key)
+        socket.send(Message_send.SerializeToString())
+        # print ("My DH is:", Message_send.message)
+
+        # receive the session key of server
+        data = socket.recv()
+        Message_rec.ParseFromString(data)
+        # print ("Server DH is:", Message_rec.message)
+
+        alice.generate_shared_secret(int (Message_rec.gb_mod_p))
+        Kas = str(alice.shared_secret)[:16].encode()
+        # print ('Kas type is:', type (Kas))
+        # print ('Length of Kas is:', len (Kas))
+        print ("Shared secret is:", int.from_bytes (Kas, sys.byteorder))
+
+        decryptor = Cipher(algorithms.AES(Kas), modes.GCM(Message_rec.iv, Message_rec.tag), backend=default_backend()).decryptor()
+        decryptor.authenticate_additional_data(Message_rec.authenticate_data)
+        decrypted_plain_text = decryptor.update(Message_rec.cipher_text) + decryptor.finalize()
+
+        #unpad
+        unpadder = padding.PKCS7(128).unpadder()
+        plain_text = unpadder.update(decrypted_plain_text) + unpadder.finalize()
+        plain_text_bytes  = plain_text
+        plain_text = plain_text.decode()
+        ## load public key
+
+        with open("public_key.der","rb") as key_file:
+            public_key = serialization.load_der_public_key(
+                key_file.read(),
+                backend=default_backend())
+
+        ##  verify the signature
+        try:
+            public_key.verify(
+                Message_rec.signature,
+                plain_text_bytes,
+                paddings.PSS(
+                    mgf=paddings.MGF1(hashes.SHA256()),
+                    salt_length=paddings.PSS.MAX_LENGTH
+                ),
+                hashes.SHA256()
+            )
+            print ("verify success!")
+        except:
+            print('Error in verifying the signature!')
+            sys.exit(1)
+
+
+
+
+        print ("Plain text is:", plain_text)
+        verify_a = plain_text.split("|")[0]
+        verify_b = plain_text.split("|")[1]
+        # print ("Decrypted Ga mod p=:",verify_a)
+        # print ("Decrypted Gb mod p=:",verify_b)
+        # print (alice.public_key)
+        # print (Message_rec.gb_mod_p)
+        if verify_a == str (alice.public_key) and verify_b == Message_rec.gb_mod_p:
+            print ("True")
+        else:
+            print ("False")
+
+    # def verify_and_login (self, socket, Kas):
+    #     Message_send = COMM_MESSAGE()
+    #     Message_send.type = COMM_MESSAGE.TYPE.LOGIN
+    #     Message_rec = COMM_MESSAGE()
+    #     data = socket.recv()
+    #     Message_rec.ParseFromString(data)
+
+
+
+
 
     def get_name(self):
         return (self.client_name, self.client_password)
 
 
 if __name__ == '__main__':
-    test_object = None
-    while True:
-        print ("Please type your Username:")
-        client_name = input()
-        print ("Please type your Password:")
-        client_password = input ()
-        test_object = Client (client_name, client_password)
-        print("The client name and password is", test_object.get_name())
-        socket = test_object.connect_to_server()
-        print ("The answer of the puzzle is:", test_object.login(socket))
+    print ("Please type your Username:")
+    client_name = input()
+    print ("Please type your Password:")
+    client_password = input ()
+    test_object = Client (client_name, client_password)
+    print("The client name and password is", test_object.get_name())
+    socket = test_object.connect_to_server()
+    puzzle = test_object.puzzle(socket)
+    print ("The answer of the puzzle is:", puzzle)
+    test_object.session(socket, puzzle)
+    # test_object.verify_and_login (socket, Kas)
 
 
         # # DH
--- server.py
+++ server.py
@@ -2,26 +2,109 @@ import socket
 from cryptography.hazmat.backends import default_backend
 from hashlib import sha256
 from message_type_pb2 import COMM_MESSAGE
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.asymmetric import padding as paddings
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
 import zmq
+import sys
+import os
+from diffiehellman.diffiehellman import DiffieHellman
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.connect("tcp://localhost:9090")
 
 Message = COMM_MESSAGE()
-while True:
+
+data = socket.recv()
+Message.ParseFromString(data)
+
+
+if Message.type == Message.TYPE.LOGIN:
+    N1 = 33
+    # Message.N1 = N1
+    digest = sha256()
+    digest.update(str(N1).encode())
+    Message.N1_hash = digest.hexdigest()
+    socket.send (Message.SerializeToString())
+
     data = socket.recv()
     Message.ParseFromString(data)
 
-    if Message.type == Message.TYPE.LOGIN:
-        N1 = 66
-        Message.N1 = N1
-        digest = sha256()
-        digest.update(str(N1).encode())
-        Message.N1_hash = digest.hexdigest()
-        socket.send (Message.SerializeToString())
+    if Message.N1 == N1:
+        print ("Puzzle figured out!")
+    else:
+        print ("Wrong!")
 
-        data = socket.recv()
-        Message.ParseFromString(data)
+    bob = DiffieHellman(group=5, key_length=200)
+    bob.generate_public_key()
+    Message.gb_mod_p = str (bob.public_key)
+    bob.generate_shared_secret(int (Message.message))
+    Kas =  str(bob.shared_secret)[:16].encode()
+    # Kas = (bob.shared_secret).to_bytes(16,sys.byteorder)
+    print ("Shared secret is:", int.from_bytes (Kas, sys.byteorder))
+    Message.gb_mod_p = str (bob.public_key)
+
+    #### loading private key
+    with open("private_key.pem", "rb") as key_file:
+        private_key = serialization.load_pem_private_key(
+            key_file.read(),
+            password=None,
+            backend=default_backend())
+
+    #### encryption
+    # print ("Ga mod p =:", Message.message)
+    # print ("Gb mod p =:", Message.gb_mod_p)
+    plain_text = Message.message + "|" + Message.gb_mod_p
+    plain_text = plain_text.encode()
+    ### sign the text
+    signature = private_key.sign(
+        plain_text,
+        paddings.PSS(
+            mgf=paddings.MGF1(hashes.SHA256()),
+            salt_length=paddings.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    Message.signature = signature
+    iv = os.urandom(16)
+    Message.iv = iv
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(plain_text)
+    padded_data += padder.finalize()
+    plain_text_padded = padded_data
+
+    authenticate_data = b'Final Project'
+    Message.authenticate_data = authenticate_data
+
+    # GCM Mode, we also need an IV
+    # encrypt
+    cipher = Cipher(algorithms.AES(Kas), modes.GCM(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    encryptor.authenticate_additional_data(authenticate_data)
+    cipher_text = encryptor.update(plain_text_padded) + encryptor.finalize()
+    # print (type (cipher_text))
+    Message.cipher_text = cipher_text
+    Message.tag = encryptor.tag
 
 
+
+
+    socket.send(Message.SerializeToString())
+
+
+
+    # print ("Length is:", len (Kas))
+    # Kas_true = bin(bob.shared_secret)[:128]
+    # print (type(Kas_true.encode()))
+    # print (Kas_true.encode())
+    #
+    # # Kas_true = int (Kas_true)
+    # print ("First 128bits is:", Kas_true)
+    # print ("Type:", type(Kas_true))
+    # print ("Length is:", len(Kas_true))
+    # print ("int is:", int.from_bytes (Kas_true.encode(), sys.byteorder))
